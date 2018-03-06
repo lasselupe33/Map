b25e32e64b05f4365cd9418db11d63a5f8dd6208
Lasse Agersten <lage@itu.dk>
2018-03-06 12:13:28 +0100

    Added canvasController

---
+++ src/controller/CanvasController.java
@@ -0,0 +1,9 @@
+package controller;
+
+
+/**
+ * This controller handles all logic and input related to the canvas that draws the map.
+ */
+public class CanvasController {
+    private m
+}
--- src/controller/KeyboardController.java
+++ src/controller/KeyboardController.java
@@ -1,4 +1,54 @@
 package controller;
 
+import java.awt.event.KeyEvent;
+
 public class KeyboardController {
+    private Model model;
+    private MainWindowView view;
+    private CanvasView canvas;
+
+    public KeyboardController(MainWindowView v, CanvasView c, Model m) {
+        view = v;
+        canvas = c;
+        model = m;
+
+        view.window.addKeyListener(this);
+        canvas.addKeyListener(this);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyChar()) {
+            case 'w':
+                canvas.pan(0, 10);
+                break;
+            case 'a':
+                canvas.pan(10, 0);
+                break;
+            case 's':
+                canvas.pan(0, -10);
+                break;
+            case 'd':
+                canvas.pan(-10, 0);
+                break;
+            case '+':
+                canvas.zoomToCenter(1.1);
+                break;
+            case '-':
+                canvas.zoomToCenter(1/1.1);
+                break;
+            case 'l':
+                canvas.toggleDrawingMode();
+                canvas.repaint();
+                break;
+            case 'o':
+                model.load("output.bin");
+                break;
+            case 'p':
+                model.save("output.bin");
+                break;
+            default:
+                break;
+        }
+    }
 }
