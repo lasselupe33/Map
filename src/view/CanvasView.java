19c3aff1e7a84a13432a1b2c7b85f25b370dfaf7
Lasse Agersten <lage@itu.dk>
2018-03-06 14:06:53 +0100

    Implemented faulty coastline drawing

--- src/view/CanvasView.java
+++ src/view/CanvasView.java
@@ -6,6 +6,9 @@ import model.osm.OSMWayType;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.NoninvertibleTransformException;
+import java.awt.geom.Rectangle2D;
 
 /**
  * This view draws the map.
@@ -13,6 +16,7 @@ import java.awt.*;
 public class CanvasView extends JComponent {
     MainModel model;
     CanvasController controller;
+    private double fps = 0.0;
 
     public CanvasView(MainModel m, CanvasController c) {
         model = m;
@@ -25,52 +29,69 @@ public class CanvasView extends JComponent {
      */
     @Override
     public void paint(Graphics _g) {
+        long t1 = System.nanoTime();
         Graphics2D g = (Graphics2D) _g;
-
-        // Start with a beige-colored screen
-        g.setPaint(new Color(255, 239, 210));
-        g.fillRect(0, 0, getWidth(), getHeight());
-
-        // Setup default stroke
         g.setStroke(new BasicStroke(Float.MIN_VALUE));
+        Rectangle2D viewRect = new Rectangle2D.Double(0, 0, getWidth(), getHeight());
 
-        // Apply transforms
+        g.setPaint(new Color(60, 149, 255));
+        g.fill(viewRect);
         g.transform(controller.getTransform());
+        try {
+            viewRect = controller.getTransform().createInverse().createTransformedShape(viewRect).getBounds2D();
+        } catch (NoninvertibleTransformException e) {
+            e.printStackTrace();
+        }
 
         if (controller.shouldAntiAlias()) {
-            g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
         }
-
-        // Draw Buildings
-        g.setPaint(new Color(200, 183, 166));
-        for (Shape building : model.get(OSMWayType.BUILDING)) {
-            g.fill(building);
+        g.setPaint(new Color(237, 237, 237));
+        for (Shape coastline: model.get(OSMWayType.COASTLINE)) {
+            g.fill(coastline);
         }
-
-        // Draw water
         g.setPaint(new Color(60, 149, 255));
-        for (Shape water : model.get(OSMWayType.WATER)) {
-            g.fill(water);
+        for (Shape water: model.get(OSMWayType.WATER)) {
+            if (water.intersects(viewRect)) {
+                g.fill(water);
+            }
         }
-
-        // Draw Unknown
         g.setPaint(Color.black);
-        for (Shape line : model.get(OSMWayType.UNKNOWN)) {
-            g.draw(line);
+        for (Shape line: model.get(OSMWayType.UNKNOWN)) {
+            if (line.intersects(viewRect)) {
+                g.draw(line);
+            }
         }
-
-        // Draw Roads
-        g.setStroke(new BasicStroke(0.00005f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER));
-        g.setPaint(new Color(255, 255, 255));
+        g.setStroke(new BasicStroke(0.00001f));
+        g.setPaint(new Color(230, 139, 213));
         for (Shape road : model.get(OSMWayType.ROAD)) {
-            g.draw(road);
+            if (road.intersects(viewRect)) {
+                g.draw(road);
+            }
         }
-
-        // Draw highways
         g.setStroke(new BasicStroke(0.00005f));
-        g.setPaint(new Color(255, 177, 90));
-        for (Shape highway : model.get(OSMWayType.HIGHWAY)) {
-            g.draw(highway);
+        g.setPaint(new Color(255, 114, 109));
+        for (Shape highway: model.get(OSMWayType.HIGHWAY)) {
+            if (highway.intersects(viewRect)) {
+                g.draw(highway);
+            }
         }
+        g.setPaint(new Color(172, 169, 151));
+        for (Shape building: model.get(OSMWayType.BUILDING)) {
+            if (building.intersects(viewRect)) {
+                g.fill(building);
+            }
+        }
+        long t2 = System.nanoTime();
+        fps = (fps + 1e9/ (t2 - t1)) / 2;
+        g.setTransform(new AffineTransform());
+        g.setColor(Color.WHITE);
+        g.fillRect(5, 5, 80, 20);
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+        g.setColor(Color.BLACK);
+        g.drawRect(5, 5, 80, 20);
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g.drawString(String.format("FPS: %.1f", fps), 10, 20);
     }
 }
