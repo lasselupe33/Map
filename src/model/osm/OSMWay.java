85fb02357ce7aa085bbc53b6ee75f27530ae6dc1
Lasse Agersten <lage@itu.dk>
2018-03-13 12:54:06 +0100

    Ensure that application will compile

--- src/model/osm/OSMWay.java
+++ src/model/osm/OSMWay.java
@@ -5,19 +5,15 @@ import java.util.ArrayList;
 public class OSMWay extends ArrayList<OSMNode>{
     private double xCoord;
     private double yCoord;
-    private ArrayList<OSMNode> nodes = new ArrayList<>();
 
     public OSMWay() {
-        OSMNode startNode = nodes.get(0);
-        xCoord = startNode.getLon();
-        yCoord = startNode.getLat();
     }
 
     public OSMNode from() {
-        return nodes.get(0);
+        return get(0);
     }
 
     public OSMNode to() {
-        return nodes.get(size() - 1);
+        return get(size() - 1);
     }
 }
