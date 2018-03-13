609f28f8e73b42c36cb7869beaba1ba2e0a460a5
Lasse Agersten <lage@itu.dk>
2018-03-13 12:29:41 +0100

    Created initial KD-tree structure

--- src/model/osm/OSMWay.java
+++ src/model/osm/OSMWay.java
@@ -3,11 +3,21 @@ package model.osm;
 import java.util.ArrayList;
 
 public class OSMWay extends ArrayList<OSMNode>{
+    private double xCoord;
+    private double yCoord;
+    private ArrayList<OSMNode> nodes = new ArrayList<>();
+
+    public OSMWay() {
+        OSMNode startNode = nodes.get(0);
+        xCoord = startNode.getLon();
+        yCoord = startNode.getLat();
+    }
+
     public OSMNode from() {
-        return get(0);
+        return nodes.get(0);
     }
 
     public OSMNode to() {
-        return get(size() - 1);
+        return nodes.get(size() - 1);
     }
 }
