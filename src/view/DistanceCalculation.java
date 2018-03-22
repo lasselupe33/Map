0e95341ffd4cce0a6c54bab0a55a98fa3a6e8ecd
Asibi Ayagiba <joay@itu.dk>
2018-03-22 10:54:00 +0100

    Implement distance calculation formula

--- src/view/DistanceCalculation.java
+++ src/view/DistanceCalculation.java
@@ -1,20 +1,19 @@
 package view;
 
 import controller.CanvasController;
-import model.MainModel;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseListener;
 import java.awt.geom.Point2D;
+
+import static java.lang.StrictMath.sqrt;
 
 public class DistanceCalculation extends JComponent {
     private CanvasController canvasController;
-    private final int x1 = 10;
-    private final int x2 = 110;
-    private double distance = 0;
-    private Point2D start;
-    private Point2D end;
+    private final int screenDistance = 100;
+    private Point2D startModelPoint;
+    private Point2D endModelPoint;
+    private double distance;
 
     public DistanceCalculation(CanvasController cc){
         canvasController = cc;
@@ -23,14 +22,29 @@ public class DistanceCalculation extends JComponent {
     public void paint(Graphics _g){
         Graphics2D g = (Graphics2D) _g;
         g.setColor(Color.red);
-        g.fillRect(0, 0, 100, 10);
-        start =  canvasController.toModelCoords(new Point(x1, 10));
-        end = canvasController.toModelCoords(new Point(x2, 10));
-        distance = end.getX()-start.getX();
+        g.fillRect(0, 0, screenDistance, 10);
+        startModelPoint =  canvasController.toModelCoords(new Point(0, 10));
+        endModelPoint = canvasController.toModelCoords(new Point(screenDistance, 10));
+        distance = getDistance(startModelPoint.getX(), startModelPoint.getY(), endModelPoint.getX(), endModelPoint.getY());
         System.out.println(distance);
 
     }
 
+    public double getDistance(double lat1, double lon1, double lat2, double lon2){
+        double R = 6371;
+        double dLat = toRadians(lat2-lat1);
+        double dLon = toRadians(lon2-lon1);
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(toRadians(lat1))*Math.cos(toRadians(lat2)) *
+                        Math.sin(dLon/2) * Math.sin(dLon/2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        double d = R * c;
+        return d;
+    }
+
+    private double toRadians(double degrees){
+        return degrees * (Math.PI/180);
+    }
 
 
 
