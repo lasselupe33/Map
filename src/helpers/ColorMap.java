06af746eb5903e9477cd312f96dc8b20245272df
Sif <sikr@itu.dk>
2018-04-04 20:59:24 +0200

    Added more colors to colorMap

--- src/helpers/ColorMap.java
+++ src/helpers/ColorMap.java
@@ -25,18 +25,30 @@ public class ColorMap {
         return standardMode.get(type);
     }
 
-
-
     private static void initializeStandard() {
         standardMode = new EnumMap<>(OSMWayType.class);
         standardMode.put(OSMWayType.COASTLINE, new Color(237, 237, 237));
-        //standardMode.put(OSMWayType.PLACE);
+        standardMode.put(OSMWayType.PLACE, new Color(242, 239, 233));
         standardMode.put(OSMWayType.WATER, new Color(60, 149, 255));
-
         standardMode.put(OSMWayType.UNKNOWN, Color.black);
-        standardMode.put(OSMWayType.ROAD, new Color(230, 139, 213));
-        standardMode.put(OSMWayType.HIGHWAY, new Color(255, 114, 109));
-        standardMode.put(OSMWayType.BUILDING, new Color(172, 169, 151));
+        standardMode.put(OSMWayType.BUILDING, new Color(223, 213, 206));
+        standardMode.put(OSMWayType.PITCH, new Color(170, 224, 203));
+        standardMode.put(OSMWayType.ALLOMENTS, new Color(238, 207, 179));
+        standardMode.put(OSMWayType.SERVICE, new Color(255, 255, 255));
+        standardMode.put(OSMWayType.ROAD, new Color(255, 255, 255));
+        standardMode.put(OSMWayType.PEDESTRIAN, new Color(255, 255, 255));
+        standardMode.put(OSMWayType.PARK, new Color(200, 250, 204));
+        standardMode.put(OSMWayType.PLAYGROUND, new Color(225, 255, 233));
+        standardMode.put(OSMWayType.CEMETERY, new Color(193, 219, 200));
+        standardMode.put(OSMWayType.FOOTWAY, new Color(250, 128, 114));
+        standardMode.put(OSMWayType.PATH, new Color(250, 128, 114));
+        standardMode.put(OSMWayType.FERRY, new Color(125, 139, 244));
+        standardMode.put(OSMWayType.SUBWAY, new Color(168, 172, 190));
+        standardMode.put(OSMWayType.CYCLEWAY, new Color(125, 139, 244));
+        standardMode.put(OSMWayType.TERTIARYROAD, new Color(255, 255, 255));
+        standardMode.put(OSMWayType.SECONDARYROAD, new Color(255, 255, 255));
+        standardMode.put(OSMWayType.HIGHWAY, new Color(252, 214, 164));
+        standardMode.put(OSMWayType.PLACE_OF_WORSHIP, new Color(175, 156, 141));
     }
 
     private static void initializeColorBlind() {
